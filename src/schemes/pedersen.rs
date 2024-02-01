5deae2679a436ffaefdb6388257ee47833415050
Mikkel Wienberg Madsen <mikkel.wienberg@alexandra.dk>
2024-02-01 14:40:09 +0100

    Couple Pedersen share and commitment

--- src/schemes/pedersen.rs
+++ src/schemes/pedersen.rs
@@ -6,9 +6,12 @@ use rand::RngCore;
 
 use crate::poly::Polynomial;
 
-use derive_more::*;
-#[derive(Add, Sub, Neg)]
-pub struct VerifiableShare<F: Field>(F, F);
+pub struct VerifiableShare<F: Field, G: Group> {
+    secret: F,
+    blindness: F,
+    commitment: Polynomial<G>,
+}
+
 pub struct Commitment<G: Send + Sync>(Polynomial<G>);
 
 /// Generators for Pedersen Secret Sharing (and Pedersen Commitments)
@@ -26,7 +29,7 @@ pub fn share<F: Field, G: Group>(
     threshold: usize,
     rng: &mut impl RngCore,
     PedersenGenParams(g, h): &PedersenGenParams<G>,
-) -> (Vec<VerifiableShare<F>>, Commitment<G>)
+) -> Vec<VerifiableShare<F, G>>
 where
     G: std::ops::Mul<F, Output = G>,
 {
@@ -36,52 +39,51 @@ where
     p1.0[0] = secret;
     // random secret `t` (left random)
 
-    let shares: Vec<VerifiableShare<F>> = ids
-        .iter()
-        .map(|i| {
-            // Sample the shares
-            VerifiableShare(p1.eval(i), p2.eval(i))
-        })
-        .collect();
-
     let commitments: Polynomial<G> =
         p1.0.iter()
             .zip(p2.0.iter())
             .map(|(&a, &b)| *g * a + *h * b)
             .collect();
-    let commitments = Commitment(commitments);
 
-    (shares, commitments)
+    let shares: Vec<_> = ids
+        .iter()
+        .map(|i| {
+            // Sample the shares
+            VerifiableShare{secret: p1.eval(i), blindness: p2.eval(i), commitment: commitments.clone()}
+        })
+        .collect();
+
+    shares
 }
 
 pub fn verify<F, G>(
     id: &F,
-    share: &VerifiableShare<F>,
-    commit: &Commitment<G>,
+    share: &VerifiableShare<F, G>,
     PedersenGenParams(g, h): &PedersenGenParams<G>,
 ) -> bool
 where
     F: Field,
     G: Group + std::ops::Mul<F, Output = G>,
 {
+    let VerifiableShare { secret, blindness, commitment } = share;
     // C0^(i^0) * C1^(i^1) * C1^(i^2) + ...
     let mut check = G::identity();
-    for (i, &a) in commit.0 .0.iter().enumerate() {
+    for (i, &a) in commitment.0.iter().enumerate() {
         check += a * id.pow([i as u64]);
     }
 
     // g^s * h^t =?= ...
-    *g * share.0 + *h * share.1 == check
+    *g * *secret + *h * *blindness == check
 }
 
-pub fn reconstruct<F: Field>(shares: &[VerifiableShare<F>], ids: &[F]) -> F {
+pub fn reconstruct<F: Field, G: Group>(shares: &[VerifiableShare<F, G>], ids: &[F]) -> F {
     // TODO: Maybe verify that the shares are all correct.
 
     // HACK: Parse as shamir, since we just use lagrange interpolation.
     let shares: Vec<_> = shares
         .iter()
         .zip(ids)
-        .map(|(share, id)| super::shamir::Share { x: *id, y: share.0 })
+        .map(|(share, id)| super::shamir::Share { x: *id, y: share.secret })
         .collect();
 
     super::shamir::reconstruct(&shares)
@@ -106,10 +108,10 @@ mod tests {
             RistrettoPoint::random(&mut rng),
         );
         let parties: Vec<_> = PARTIES.map(Scalar::from).collect();
-        let (shares, commit) = share::<Scalar, RistrettoPoint>(v, &parties, 2, &mut rng, &gens);
+        let shares = share::<Scalar, RistrettoPoint>(v, &parties, 2, &mut rng, &gens);
 
         for (i, share) in parties.iter().zip(shares.iter()) {
-            assert!(verify(i, share, &commit, &gens));
+            assert!(verify(i, share, &gens));
         }
 
         let v2 = reconstruct(&shares, &parties);
