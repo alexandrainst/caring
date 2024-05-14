db5908b5abf0066d1b1f68968c4788e6d59bfb6d
Mikkel Wienberg Madsen <mikkel.wienberg@alexandra.dk>
2024-05-14 14:30:20 +0200

    Add lasso-addm example

---
+++ pycare/examples/lasso1.py
@@ -0,0 +1,30 @@
+from addm import lasso_ADMM
+import caring
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+
+n_samples, n_features = 100, 100
+A = np.random.randn(n_samples, n_features)
+w = np.random.randn(n_features)
+b = A.dot(w) + np.random.randn(n_samples)
+
+# .. make b be {-1, 1} since its a classification problem ..
+b = np.sign(A.dot(w) + np.random.randn(n_samples))
+
+
+A_1, A_2 = np.array_split(A, 2)
+b_1, b_2 = np.array_split(b, 2)
+
+
+engine = caring.setup("127.0.0.1:1234", "127.0.0.1:1235")
+theta_1, func_vals = lasso_ADMM(engine, A_1, b_1)
+# lets plot the objective values of the function
+# to make sure it has converged
+plt.plot(func_vals)
+plt.ylabel('function values')
+plt.xlabel('iterations')
+
+plt.grid()
+plt.show()
